9c7e0509c8313d3de77dc19dc40404b9694993fb
TN Botz <77948100+rubandurai27@users.noreply.github.com>
2021-10-31 14:30:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,15 +11,18 @@ import subprocess
 d = os.environ.get("d")
 uid = os.environ.get("uid")
 client = TelegramClient('anfghohn',int(os.environ.get("a")),os.environ.get("b")).start(bot_token=os.environ.get("c"))
+
 @client.on(events.NewMessage(pattern='(?i)/start'))
 async def handler(event):
     chat = await event.get_chat()
     await client.send_message(chat, "example /upload file.mp4[filename] link will expire depends on storage weight")
+
 @client.on(events.NewMessage(pattern='/diskusage'))
 async def handler(event):
     chat = await event.get_chat()
     stat = shutil.disk_usage("/app/templates/download")
     await client.send_message(chat,str(stat))     
+
 @client.on(events.NewMessage(pattern='/url'))
 async def handler(event):
     link =event.text.split(' ')[1]
@@ -28,15 +31,16 @@ async def handler(event):
     s = f"http://linkapi.net/open/create_item?api_key={uid}&content_src={link}&link_type=link&title={l}"
     r = requests.get(s).json()
     z=r['data']["item_id"]
-    markup  = client.build_reply_markup(Button.url(" 🔥url🔥",f"http://m.pdisk.net/share-video?videoid={z}"))
+    markup  = client.build_reply_markup(Button.url("⚡ PDISK LINK ⚡",f"http://m.pdisk.net/share-video?videoid={z}"))
     await client.send_message(chat, "link will working depends on size it takes half or more ... ", buttons=markup)
+
 @client.on(events.NewMessage(pattern='/telepdisk'))
 async def handler(event):
     chat = await event.get_chat()
     print(chat)
     dw = await event.get_reply_message()
     links =event.text.split(" ")[1]
-    await client.send_message(chat,"🌠 downloading 🌠")
+    await client.send_message(chat,"DOWNLOADING ...")
     ss=await dw.download_media(links)
     shutil.move(f"/app/{links}",f"/app/templates/download/{links}")
     await client.send_message(chat,f"wait few minutes ...{links}")
@@ -54,16 +58,17 @@ async def handler(event):
     #z=r['data']["item_id"]
    # await event.delete()
    # client.delete_messages()
-    markup  = client.build_reply_markup(Button.url(" 🔥url🔥",f"http://m.pdisk.net/share-video?videoid={f}"))
+    markup  = client.build_reply_markup(Button.url("⚡ PDISK LINK⚡",f"http://m.pdisk.net/share-video?videoid={f}"))
     await client.send_message(chat, "link will working depends on size it takes half or more ... ", buttons=markup)
     #os.remove(f"/app/templates/download/{links}")  
+
 @client.on(events.NewMessage(pattern='(?i)/upload'))
 async def handler(event):
     chat = await event.get_chat()
     print(chat)
     dw = await event.get_reply_message()
     links =event.text.split(" ")[1]
-    await client.send_message(chat,"🌠 downloading 🌠")
+    await client.send_message(chat,"DOWNLOADING ...")
     ss=await dw.download_media(links)
     shutil.move(f"/app/{links}",f"/app/templates/download/{links}")
     await client.send_message(chat,f"{d}/files/{links}")
